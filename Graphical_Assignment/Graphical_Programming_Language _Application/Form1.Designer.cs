35dd1a413aca72574f157cf67fed712ce88a9a2c
Alish Shrestha <alizhshrestha@gmail.com>
2018-12-30 23:13:04 +0545

    inserted picturebox image and move to methods

--- Graphical_Assignment/Graphical_Programming_Language _Application/Form1.Designer.cs
+++ Graphical_Assignment/Graphical_Programming_Language _Application/Form1.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.rtxt_code = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.rtxt_split = new System.Windows.Forms.RichTextBox();
             this.btn_split = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // rtxt_code
@@ -44,6 +48,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(768, 343);
@@ -68,6 +73,15 @@
             this.btn_split.UseVisualStyleBackColor = true;
             this.btn_split.Click += new System.EventHandler(this.btn_split_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -80,6 +94,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -90,6 +106,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.RichTextBox rtxt_split;
         private System.Windows.Forms.Button btn_split;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Graphical_Assignment/Graphical_Programming_Language _Application/Form1.cs
+++ Graphical_Assignment/Graphical_Programming_Language _Application/Form1.cs
@@ -13,145 +13,255 @@ namespace Graphical_Programming_Language__Application
 {
     public partial class Form1 : Form
     {
-
+        //Declaration
         Shape shape1, shape2;
-        Boolean drawCircle, drawRect;
+        Boolean drawCircle, drawRect, movePointer;
         String program;
-        List<int> parameterList;
+        String[] words;
+        List<int> circleParameterList, rectangleParameterList;
+        int moveX, moveY;
 
 
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
+            //Shape creation and initialization
             AbstractFactory shapeFactory = FactoryProducer.getFactory("Shape");
             shape1 = shapeFactory.getShape("Circle");
             shape2 = shapeFactory.getShape("Rectangle");
         }
 
+        /// <summary>
+        /// Form load method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
             
 
         }
 
+        /// <summary>
+        /// button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_split_Click(object sender, EventArgs e)
         {
-            //whole written program
-            program = rtxt_code.Text;
 
-
-            // ... Parts are separated by Windows line breaks.
-
-            // Use a char array of 2 characters (\r and \n).
-            // ... Break lines into separate strings.
-            // ... Use RemoveEntryEntries so empty strings are not added.
-            char[] delimiters = new char[] { '\r', '\n' };
-            string[] parts = program.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-
-            //loop through the whole program code line
-            for (int i = 0; i < parts.Length; i++)
+            try
             {
-                //single code line
-                String code_line = parts[i];
 
-                //words of the code line
-                String[] words = code_line.Split(' ');
-               
-
-                MessageBox.Show("Whole words length: " + Convert.ToString(parts.Length));
+                //whole written program
+                program = rtxt_code.Text;
 
 
-                MessageBox.Show("code lengths: " + Convert.ToString(words.Length));
-                Code_Attributes code_attributes = new Code_Attributes();
-                code_attributes.setCommand(words[0]);
-                code_attributes.setObj(words[1]);
-                code_attributes.setVariableName(words[2]);
+                // ... Parts are separated by Windows line breaks.
 
-                parameterList = new List<int>();
+                // Use a char array of 2 characters (\r and \n).
+                // ... Break lines into separate strings.
+                // ... Use RemoveEntryEntries so empty strings are not added.
+                char[] delimiters = new char[] { '\r', '\n' };
+                string[] parts = program.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-                
-
-                //for storing parameters value in int array
-                for (int j = 3; j<words.Length;j++)
+                //loop through the whole program code line
+                for (int i = 0; i < parts.Length; i++)
                 {
-                    int parameter = Convert.ToInt32(words[j]);
+                    //single code line
+                    String code_line = parts[i];
 
-                    MessageBox.Show("Parameter " + j.ToString() + parameter.ToString());
-
-                    parameterList.Add(parameter);
-                }
-
-                for (int z = 0; z < parameterList.Count; z++)
-                {
-                    MessageBox.Show("Count No: " + z);
-                }
+                    //words of the code line
+                    words = code_line.Split(' ');
 
 
+                    //MessageBox.Show("Whole words length: " + Convert.ToString(parts.Length));
 
-                foreach (int parameterLists in parameterList)
-                {
+
+                    //MessageBox.Show("code lengths: " + Convert.ToString(words.Length));
+
+                    //Code attribute class initialization to hold code line information
+                    /*
+                    Code_Attributes code_attributes = new Code_Attributes();
+                    code_attributes.setCommand(words[0]);
+                    code_attributes.setObj(words[1]);
+                    code_attributes.setVariableName(words[2]);
+                    */
+
+                    /*
+                    for (int z = 0; z < parameterList.Count; z++)
+                    {
+                        MessageBox.Show("Count No: " + z);
+                    }
+
+
+
+
+                    foreach (int parameterLists in parameterList)
+                    {
+                        MessageBox.Show("Parameter no: " + parameterLists.ToString());
+                    }
+                    */
                     
-                    MessageBox.Show("Parameter list be like: " + parameterLists.ToString());
-                }
 
 
 
-                String param = words[3];
-                MessageBox.Show("command: " + code_attributes.getCommand() + "\r\n" + "object: " + code_attributes.getObj() + "\r\n" + "variable_name: " + code_attributes.getVariableName() + "\r\n" + "parameter: " + " "
-                    );
-                if (words[0]=="draw")
-                {
-                    if (words[1] == "circle")
+                    //MessageBox.Show("command: " + code_attributes.getCommand() + "\r\n" + "object: " + code_attributes.getObj() + "\r\n" + "variable_name: " + code_attributes.getVariableName() + "\r\n" + "parameter: " + " "
+                    //    );
+
+                    if (words[0] == "draw")
                     {
+                        if (words[1] == "circle")
+                        {
 
-                        code_attributes.setParameter(new int[] { });
+                            //List to hold parameter info
+                            circleParameterList = new List<int>();
+
+
+
+                            //for storing parameters value in int array
+                            for (int j = 3; j < words.Length; j++)
+                            {
+                                int parameter = Convert.ToInt32(words[j]);
+
+                                //MessageBox.Show("Parameter " + j.ToString() + parameter.ToString());
+                                if (circleParameterList.Count == 0)
+                                {
+                                    MessageBox.Show("circle parameter empty........ adding");
+                                    circleParameterList.Add(parameter);
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("circle parameter clearing");
+                                    circleParameterList.Clear();
+                                }
+                                
+                                
+                            }
+
+                            
+
+                            if (words.Length>4)
+                            {
+                                MessageBox.Show("can't draw");
+                            }
+                            else
+                            {
+                                drawCircle = true;
+                                MessageBox.Show("Draw Circle: " + Convert.ToString(drawCircle));
+                                panel1.Refresh();
+                                //rectangleParameterList.Clear();
+
+                                //circleParameterList.Clear();
+                            }
+
+
+
+
+                        }
+
+                        if (words[1] == "rectangle")
+                        {
+                            
+                            //List to hold parameter info
+                            rectangleParameterList = new List<int>();
+                           
+
+
+                            //for storing parameters value in int array
+                            for (int j = 3; j < words.Length; j++)
+                            {
+                                int parameter = Convert.ToInt32(words[j]);
+
+                                //MessageBox.Show("Parameter " + j.ToString() + parameter.ToString());
+                                
+
+                                MessageBox.Show("rectangle parameter empty........ adding");
+                                 rectangleParameterList.Add(parameter);
+                                MessageBox.Show("Rectangle parameter list be like: " + rectangleParameterList.ElementAt(j-3));
+
+                            }
+
+
+                            if (words.Length > 5)
+                            {
+                                MessageBox.Show("can't draw");
+                            }
+                            else
+                            {
+                                drawRect = true;
+                                MessageBox.Show("Draw Rectangle: " + Convert.ToString(drawRect));
+                                panel1.Refresh();
+
+                                //rectangleParameterList.Clear();
+                            }
+                        }
                         
-
-                        if (parameterList.Count== 1)
-                        {
-                            drawCircle = true;
-                            MessageBox.Show("Draw Circle: " + Convert.ToString(drawCircle));
-                        }
-                        else
-                        {
-                            MessageBox.Show("Parameter passed is incorrect");
-                        }
-
-
+                        
                     }
 
-                    if (words[1] == "rectangle")
+                    if (words[0]=="move")
                     {
 
-                        if (parameterList.Count == 2)
+                        moveX = Convert.ToInt32(words[1]);
+                        moveY = Convert.ToInt32(words[2]);
+
+                        if (words.Length>3)
                         {
-                            drawRect = true;
-                            MessageBox.Show("Draw Rectangle: " + Convert.ToString(drawRect));
+                            MessageBox.Show("Please enter correrct parameters");
                         }
                         else
                         {
-                            MessageBox.Show("Parameter passed is incorrect");
+                            movePointer = true;
+                            panel1.Refresh();
                         }
+                        
                     }
-                    panel1.Refresh();
+
                 }
-                
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("!!Please input correct code or syntax!!");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("!!Please input correct parameter!!");
             }
 
         }
 
+        /// <summary>
+        /// Panel paint method to paint to the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            
             Graphics g = e.Graphics;
-            MessageBox.Show("Painting...");
+            //MessageBox.Show("Painting...");
             if (drawCircle==true)
             {
                 Circle c = new Circle();
-                c.setRadius(parameterList[0]);
+                //c.setRadius(parameterList[0]);
+                c.setRadius(Convert.ToInt32(circleParameterList[0]));
+                /*
+                 * parameterList.Clear();
+
+                foreach (int parameterLists in parameterList)
+                {
+                    MessageBox.Show("Parameter list be like: " + parameterLists.ToString());
+                }
+                */
+
                 c.setX(200);
                 c.setY(200);
-                MessageBox.Show("Radius is: " + parameterList[0].ToString());
+                //MessageBox.Show("Radius is: " + parameterList[0].ToString());
                 c.draw(g);
             }
 
@@ -160,11 +270,29 @@ namespace Graphical_Programming_Language__Application
                 Rectangle rect = new Rectangle();
                 rect.setX(100);
                 rect.setY(100);
-                rect.setWidth(parameterList[0]);
-                MessageBox.Show("Width is: " + parameterList[0].ToString());
-                rect.setHeight(parameterList[1]);
-                MessageBox.Show("Height is: " + parameterList[0].ToString());
+                //rect.setWidth(parameterList[0]);
+                //rect.setHeight(parameterList[1]);
+                //MessageBox.Show("Rectangle param1: " + words[3]+ "Rectangle param2: " + words[4]);
+                rect.setWidth(Convert.ToInt32(rectangleParameterList[0]));
+                rect.setHeight(Convert.ToInt32(rectangleParameterList[1]));
+
+                /*
+                 * parameterList.Clear();
+
+                foreach (int parameterLists in parameterList)
+                {
+                    MessageBox.Show("Parameter list be like: " + parameterLists.ToString());
+                }
+                */
+
+                //MessageBox.Show("Width is: " + parameterList[0].ToString());
+                //MessageBox.Show("Height is: " + parameterList[0].ToString());
                 rect.draw(g);
+            }
+            if (movePointer==true)
+            {
+                Point point = new Point(moveX,moveY);
+                pictureBox1.Location = point;
             }
             
         }
